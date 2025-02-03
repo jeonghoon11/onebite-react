cf5d9e20d143bd6f9540141966f251619057b5d2
jeonghoon11 <jih361@naver.com>
2025-02-03 17:46:24 +0900

    25.02.03(월) useReducer

---
+++ section09/src/components/TodoItem.jsx
@@ -0,0 +1,27 @@
+import "./TodoItem.css";
+
+const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
+  const onChangeCheckbox = () => {
+    onUpdate(id);
+  };
+
+  const onClickDeleteButton = () => {
+    onDelete(id);
+  };
+
+  return (
+    <div className="TodoItem">
+      <input
+        onChange={onChangeCheckbox} //button이 아닌 input태그이기 때문에 onChange를 사용
+        readOnly
+        checked={isDone}
+        type="checkbox"
+      />
+      <div className="content">{content}</div>
+      <div className="date">{new Date(date).toLocaleDateString()}</div>
+      <button onClick={onClickDeleteButton}>삭제</button>
+    </div>
+  );
+};
+
+export default TodoItem;
